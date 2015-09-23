5a41801fa30dce8d3f03e71985b5ed868cf88bd1
Wandick Vinícius Antunes Lopes <vianalista@gmail.com>
2015-09-23 15:55:02 -0300

    Net Beans project files.

--- ViniLibCpp/nbproject/Makefile-Release.mk
+++ ViniLibCpp/nbproject/Makefile-Release.mk
@@ -35,6 +35,7 @@ OBJECTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}
 
 # Object Files
 OBJECTFILES= \
+	${OBJECTDIR}/OverloadedFunction.o \
 	${OBJECTDIR}/main.o \
 	${OBJECTDIR}/menu.o
 
@@ -62,6 +63,11 @@ LDLIBSOPTIONS=
 ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/vinilibcpp: ${OBJECTFILES}
 	${MKDIR} -p ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}
 	${LINK.cc} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/vinilibcpp ${OBJECTFILES} ${LDLIBSOPTIONS}
+
+${OBJECTDIR}/OverloadedFunction.o: OverloadedFunction.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} "$@.d"
+	$(COMPILE.cc) -O2 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/OverloadedFunction.o OverloadedFunction.cpp
 
 ${OBJECTDIR}/main.o: main.cpp 
 	${MKDIR} -p ${OBJECTDIR}
