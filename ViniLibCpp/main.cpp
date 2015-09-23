cdb143f20d660900b856580a59ca58622b7f1e3a
Wandick Vinícius Antunes Lopes <vianalista@gmail.com>
2015-09-23 10:54:03 -0300

    Start project.

---
+++ ViniLibCpp/main.cpp
@@ -0,0 +1,23 @@
+/* 
+ * File:   main.cpp
+ * Author: vinicius
+ *
+ * Created on 23 de Setembro de 2015, 10:41
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    cout << "===  Project ViniLibCpp  ===" << endl;
+    cout << "=== Start call librarys  ===" << endl;
+    
+    cout << "===  End call librarys   ===" << endl;
+    return 0;
+}
+
